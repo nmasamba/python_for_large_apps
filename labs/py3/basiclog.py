bc2510e5db3bd9a49a9dc1292c40b6e3f3ed639a
nmasamba <slimm4rl@yahoo.com>
2017-10-26 16:06:05 +0100

    python for large apps

---
+++ labs/py3/basiclog.py
@@ -0,0 +1,15 @@
+mode = 'development'
+
+log_file = 'myapp.log'
+
+if mode == 'development':
+	log_level = logging.DEBUG
+	log_mode = 'w'
+else:
+	log_level = logging.WARNING
+	log_mode = 'a'
+
+logging.basicConfig(level=log_level, filename=log_file, filemode=log_mode)
+logging.debug('debug message')
+logging.warning('look out!')
+logging.critical('we have a problem here')
